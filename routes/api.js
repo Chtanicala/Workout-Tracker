3c553d52424a1b9bc898b1dc9eec27a3ce148c31
Christian Tanicala <christian.tanicala@gmail.com>
2022-01-08 18:24:40 -0500

    added routers file

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,43 @@
-const router = require("express").Router();
-const Exercise = require("../models/exercise.js");
+const router = rqeuire('express').Router();
+const db = require('../models');
+
+router.get('/workouts', async (req, res) => {
+    const workouts = await db.Workout.aggregate([
+        {$addFields: {totalDuration: {$sum: '$exercises.duration'}}}
+    ]);
+
+    res.json(workouts);
+})
+
+router.get('/workouts/range', async (req, res) => {
+    const workoutData = await db.Workout.aggregate([
+        {$sort: {day: -1}},
+        {$limit: 7},
+        {$addFields: {totalDuration: {$sum: '$exercises.duration'}}}
+    ]);
+
+    res.json(workoutData);
+})
+
+router.post('/workouts', async (req, res) => {
+    const addWorkout = await db.Workout.create(req.body);
+
+    res.json(addWorkout)
+});
+
+router.put('/workouts/:id', async (req, res) => {
+    Workout.findOne({_id: req.params.id})
+        .then(workout => {
+            workout.exericses.push(req.body)
+            workout.save((error) => {
+                if (error) {
+                    res.status(400).json(error);
+                }
+                res.json(workout)
+            })
+        })
+})
+
+
 
 module.exports = router;
